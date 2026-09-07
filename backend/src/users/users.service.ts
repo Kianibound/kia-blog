a52bed3eb5761afbffd9c82ba5515be7eb8c9d39
kianibound <kianibound@gmail.com>
2026-09-07 12:39:06 +0330

    Add createUserDto, user controller, and user service for creating a user

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
+
   async findAll() {
     return this.prisma.user.findMany();
   }
+
+async create(dto: CreateUserDto) {
+  return this.prisma.user.create({
+    data: dto,
+  });
+}
 }
